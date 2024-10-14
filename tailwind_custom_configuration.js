89279feff62428862ae17c1f1abe1e64eb4c05e7
mohammadsajjadhossain9648 <sajjadhossain9648@gmail.com>
2024-10-14 16:25:30 +0600

    Initial project setup done

---
+++ tailwind_custom_configuration.js
@@ -0,0 +1,26 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          white_color: 'rgb(255, 255, 255)',
+          shade_white_color: 'rgb(244, 244, 244)',
+
+          black_color: 'rgb(28, 28, 28)',
+          shade_black_color: 'rgb(73, 73, 73)',
+
+          grey_color_1: 'rgb(119, 119, 119)',
+          grey_color_2: 'rgb(93, 90, 111)',
+
+          green_color: 'rgb(14, 122, 129)',
+
+          gradient_color: 'linear-gradient(270.00deg, rgb(255, 0, 0) 0%, rgb(255, 137, 56) 100%)',
+
+          bg_featured_color: 'linear-gradient(180.00deg, rgb(244, 244, 244),rgba(244, 244, 244, 0) 100%)',
+          bg_color_1: 'rgba(87, 118, 57, 0.1)',
+          bg_color_2: 'rgba(230, 166, 35, 0.1)',
+          bg_color_3: 'linear-gradient(90.00deg, rgb(217, 217, 217), rgba(255, 255, 255, 0) 100%)',
+          bg_footer_color: 'linear-gradient(270.00deg, rgba(255, 0, 0, 0.1), rgba(255, 137, 56, 0.1) 100%);',
+        }
+      }
+    }
+  }
